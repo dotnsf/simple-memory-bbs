d1936ccdd2a0744eb2b5d78e35a8fe4c17377c7d
K.Kimura <dotnsf@gmail.com>
2024-05-11 21:33:25 +0900

    __mycaptcha_formula__ handled.

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.set( 'view engine', 'ejs' );
 
 app.get( '/', function( req, res ){
   db.readItems().then( function( r ){
+    if( r && r.items ){
+      for( var i = 0; i < r.items.length; i ++ ){
+        var c = r.items[i].created;
+        if( typeof c == 'string' ){ c = parseInt( c ); }
+        r.items[i].created = datetime2string( c );
+      }
+    }
+    console.log( JSON.stringify( r, null, 2 ) );
     res.render( 'index', r );
   });
 });
@@ -31,6 +39,23 @@ app.post( '/message', function( req, res ){
     res.redirect( '/' );
   });
 });
+
+function datetime2string( c ){
+  var t = ( c ? c : ( new Date() ).getTime() );
+  var dt = new Date();
+  dt.setTime( t );
+
+  var yyyy = dt.getFullYear();
+  var mm = dt.getMonth() + 1;
+  var dd = dt.getDate();
+  var hh = dt.getHours();
+  var nn = dt.getMinutes();
+  var ss = dt.getSeconds();
+
+  var str = yyyy + '-' + ( mm < 10 ? '0' : '' ) + mm + '-' + ( dd < 10 ? '0' : '' ) + dd
+    + ' ' + ( hh < 10 ? '0' : '' ) + hh + ':' + ( nn < 10 ? '0' : '' ) + nn + ':' + ( ss < 10 ? '0' : '' ) + ss;
+  return str;
+}
 
 var port = process.env.port || 8080;
 app.listen( port );
